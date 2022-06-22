71dd7e1827a3d2265d30b6ac959729aa271c7d4d
Evgeniy Alipatov <ealipatov@gmail.com>
2022-06-22 10:46:27 +0300

    Урок 1. Добавлена кнопка в разметку и повешена на неё clickListener в Activity.

--- app/src/main/java/by/ealipatov/kotlin/apponkotlin/MainActivity.kt
+++ app/src/main/java/by/ealipatov/kotlin/apponkotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package by.ealipatov.kotlin.apponkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text : TextView = findViewById(R.id.title_text)
+
+        findViewById<AppCompatButton>(R.id.btn_test).setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                text.text = getString(R.string.btn_is_pressed)
+            }
+        })
     }
 }
